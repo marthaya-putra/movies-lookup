0026007a18767062de10436b1042edbd4dcadb01
Marthayaputra <marthayaputra@hotmail.com>
2024-02-01 11:23:16 +0800

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "./ui/header/header";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
             <Header />
           </div>
           <div style={{ marginTop: "48px" }}>{children}</div>
+          <Analytics />
         </div>
       </body>
     </html>
